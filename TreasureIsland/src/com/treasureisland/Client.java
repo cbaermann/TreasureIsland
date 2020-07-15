78c3587fe09e3d53a72b16960d325ca65cc6ac77
Cody Baermann <cbaermann@comcast.net>
2020-07-15 14:19:58 -0700

    title decorator and getting scanner to register player name

---
+++ TreasureIsland/src/com/treasureisland/Client.java
@@ -0,0 +1,8 @@
+package com.treasureisland;
+
+public class Client {
+    public static void main(String[] args) {
+        TreasureIslandGameScanner beginGame = new TreasureIslandGameScanner();
+        beginGame.chosePlayerName();
+    }
+}
