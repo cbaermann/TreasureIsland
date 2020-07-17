87287b5fcd1e75e34b4d10fe089aa5fe0b0b3588
Cody Baermann <cbaermann@comcast.net>
2020-07-17 09:03:02 -0700

    adding basic functionality to each area w/in island

--- TreasureIsland/src/com/treasureisland/rumrummersisle/Docks.java
+++ TreasureIsland/src/com/treasureisland/rumrummersisle/Docks.java
@@ -1,11 +1,16 @@
 package com.treasureisland.rumrummersisle;
 
-public class Docks extends Location{
+public class Docks implements Location{
     private String locationName = "Docks";
 
     @Override
     public String getLocationName() {
         return locationName;
+    }
+
+    @Override
+    public void talkToNPC() {
+        System.out.println("youre talking to npc in docks");
     }
 
 }
--- TreasureIsland/src/com/treasureisland/rumrummersisle/MasterBrewersHouse.java
+++ TreasureIsland/src/com/treasureisland/rumrummersisle/MasterBrewersHouse.java
@@ -1,10 +1,15 @@
 package com.treasureisland.rumrummersisle;
 
-public class MasterBrewersHouse extends Location{
+public class MasterBrewersHouse implements Location{
     private String locationName = "Master Brewers House";
     @Override
     public String getLocationName() {
         return locationName;
+    }
+
+    @Override
+    public void talkToNPC() {
+        System.out.println("you're talking to npc in master brewers house");
     }
 
 
