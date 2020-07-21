6965fdebadadb47fa859a2e176763626fc819d8b
kushkota <kushkota@gmail.com>
2020-07-21 10:36:25 -0700

    Second Commit: Added Test

--- TreasureIsland/test/com/treasureisland/world/SugarCaneFieldTest.java
+++ TreasureIsland/test/com/treasureisland/world/SugarCaneFieldTest.java
@@ -1,5 +1,23 @@
+package com.treasureisland.world;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SugarCaneFieldTest {
 
+    @Test
+    public void getLocationName() {
+
+
+        // Arrange
+        var sugarCaneField = new SugarCaneField();
+
+        // Act
+        var expected = "Sugar cane field";
+        var actual = sugarCaneField.getLocationName();
+
+        // Assert
+        assertEquals(expected, actual);
+    }
 }
